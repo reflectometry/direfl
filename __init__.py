71b27e1aef0c06514842b9e4793a36fdd61bc17f
jkrycka <jkrycka@a92bf6fb-5b16-0410-9b54-b8becc4ab72c>
2009-11-22 01:45:48 +0000

    inversion: update description

--- __init__.py
+++ __init__.py
@@ -3,21 +3,20 @@
 """
 Surround variation reflectometry
 
-The surround variation reflectometry package consists of three
-interacting pieces: simulatation, phase reconstruction
-and phase inversion.  Before the experiment starts, you should
-provide an expected profile to the simulator.  This will indicate
-whether inversion is feasible for the given structure, and allow
-you to determine noise sensitivity and the effects of various
-substrate and surround media.  Once you are comfortable with the
-parameters, you can perform your experiment by back reflectivity
-through the film on a substrate, once with one surround variation
-on the surface, and again with another.  This returns a real and
-imaginary reflectivity for your sample as a reversed film with the
-substrate material on either side.  Next you perform inversion
-on the real portion of the reflection amplitude, which returns
-the scattering length density profile.  This profile can then be
-used to compute the expected reflectivity for the original
+The surround variation reflectometry package consists of three interacting
+pieces: simulation, phase reconstruction and phase inversion.  Before the
+experiment starts, you should provide an expected profile to the simulator.
+This will indicate whether inversion is feasible for the given structure, and
+allow you to determine noise sensitivity and the effects of various substrate
+and surround media.  Once you are comfortable with the parameters, you can
+perform your experiment by back reflectivity through the film on a substrate.
+Two experimental runs are needed where the only difference in the setup is that
+one of the surround materials (either the incident or substrate layer) is
+changed.  This generates two datasets each containing a real and an imaginary
+reflectivity for your sample as a reversed film with the substrate material on
+either side.  Next you perform inversion on the real portion of the reflection
+amplitude which returns the scattering length density profile.  This profile
+can then be used to compute the expected reflectivity for the original
 measurements, which if all goes well, should agree prefectly.
 
 See :class:`core.Inversion`, :func:`core.reconstruct` and
