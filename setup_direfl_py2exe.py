687916fe50c41e701f3b4c86bbb9bc5445271b9c
jkrycka <jkrycka@a92bf6fb-5b16-0410-9b54-b8becc4ab72c>
2010-08-03 16:40:06 +0000

    inversion: refactor build script and add comments in particular about matplotlib

--- setup_direfl_py2exe.py
+++ setup_direfl_py2exe.py
@@ -23,25 +23,19 @@
 # Author: James Krycka
 
 """
-Create direfl.exe in the inversion\dist directory tree using py2exe.  This
-executable contains the python runtime environment, required python packages,
-and the DiRefl application.  Additional resource files needed to run DiRefl are
-placed in the dist directory tree.  On completion, the contents of the dist
-directory tree can be used by the Inno Setup Compiler to build a Windows
-installer/uninstaller for the DiRefl application.
+This script creates direfl.exe in inversion\dist using py2exe.
+
+The resulting executable bundles the DiRefl application, the python runtime
+environment, and other required python packages into a single file.  Additional
+resource files that are needed when DiRefl is run are placed in the dist
+directory tree.  On completion, the contents of the dist directory tree can be
+used by the Inno Setup Compiler (via a separate script) to build a Windows
+installer/uninstaller for deployment of the DiRefl application.  For testing
+purposes, direfl.exe can be run from the dist directory.
 """
 
 import os
 import sys
-import glob
-
-from distutils.core import setup
-import py2exe  # augment setup interface with the py2exe command
-
-if len(sys.argv) == 1:
-    sys.argv.append('py2exe')
-
-import matplotlib
 
 '''
 print "*** Python path is:"
@@ -49,7 +43,18 @@ for i, p in enumerate(sys.path):
     print "%5d  %s" %(i, p)
 '''
 
-# Retrieve version information.
+from distutils.core import setup
+
+# Augment the setup interface with the py2exe command and make sure the py2exe
+# option is passed to setup.
+import py2exe
+
+if len(sys.argv) == 1:
+    sys.argv.append('py2exe')
+
+import matplotlib
+
+# Retrieve the application version string.
 from version import version as version
 
 # Create a manifest for use with Python 2.5 on Windows XP.  This manifest is
@@ -57,6 +62,8 @@ from version import version as version
 # image directory) when wxPython is included so that the Windows XP theme is
 # used when rendering wx widgets.  The manifest below is adapted from the
 # Python manifest file (C:\Python25\pythonw.exe.manifest).
+#
+# Note that a different manifest is required if using another version of Python.
 
 manifest = """
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
@@ -83,64 +90,35 @@ manifest = """
 </assembly>
 """
 
-class Target():
-    def __init__(self, **kw):
-        # Metadata about the distribution
-        self.__dict__.update(kw)
-        self.version = version
-        self.company_name = "University of Maryland"
-        self.copyright = "BSD style copyright"
-        self.name = "DiRefl"
-
-# Create a list of all files to include with the executable being built in the
-# dist directory tree.
+# Create a list of all files to include along side the executable being built
+# in the dist directory tree.  Each element of the data_files list is a tuple
+# consisting of a path (relative to dist\) and a list of files in that path.
 data_files = []
 
-# Add matplotlib data files from the mpl-data folder and its subfolders.
-# The compiled program will look for these files in the \mpl-data ...
-# Note that the location of these data files varies dependnig on the version of
-# MatPlotLib that is being used.
-# Note that glob will not find files without an extension such as matplotlibrc.
-#
-# The technique for obtaining MatPlotLib auxliliary files was adapted from the
-# examples and discussion on http://www.py2exe.org/index.cgi/MatPlotLib.
-matplotlibdatadir = matplotlib.get_data_path()
-mplData = ('mpl-data', glob.glob(os.path.join(matplotlibdatadir,'*.*')))
-data_files.append(mplData)
-mplData = ('mpl-data', [os.path.join(matplotlibdatadir,'matplotlibrc')])
-data_files.append(mplData)
-mplData = (r'mpl-data\images',
-           glob.glob(os.path.join(matplotlibdatadir,r'images\*.*')))
-data_files.append(mplData)
-mplData = (r'mpl-data\fonts\afm',
-           glob.glob(os.path.join(matplotlibdatadir,r'fonts\afm\*.*')))
-data_files.append(mplData)
-mplData = (r'mpl-data\fonts\pdfcorefonts',
-           glob.glob(os.path.join(matplotlibdatadir,r'fonts\pdfcorefonts\*.*')))
-data_files.append(mplData)
-mplData = (r'mpl-data\fonts\ttf',
-           glob.glob(os.path.join(matplotlibdatadir,r'fonts\ttf\*.*')))
-data_files.append(mplData)
+# Add data files from the matplotlib\mpl-data folder and its subfolders.
+# For matploblib prior to version 0.99 see the examples at the end of the file.
+data_files = matplotlib.get_py2exe_datafiles()
 
-# Add data files that need to reside in the same directory as the image.
-data_files.append( ('.', [os.path.join('.','demo_model_1.dat')]) )
-data_files.append( ('.', [os.path.join('.','demo_model_2.dat')]) )
-data_files.append( ('.', [os.path.join('.','demo_model_3.dat')]) )
-data_files.append( ('.', [os.path.join('.','direfl.ico')]) )
-data_files.append( ('.', [os.path.join('.','LICENSE-direfl.txt')]) )
-data_files.append( ('.', [os.path.join('.','qrd1.refl')]) )
-data_files.append( ('.', [os.path.join('.','qrd2.refl')]) )
-data_files.append( ('.', [os.path.join('.','surround_air_4.refl')]) )
-data_files.append( ('.', [os.path.join('.','surround_d2o_4.refl')]) )
-data_files.append( ('.', [os.path.join('.','README-direfl.txt')]) )
-data_files.append( ('.', [os.path.join('.','splash.png')]) )
+# Add resource files that need to reside in the same directory as the image.
+data_files.append( ('.', [os.path.join('.', 'demo_model_1.dat')]) )
+data_files.append( ('.', [os.path.join('.', 'demo_model_2.dat')]) )
+data_files.append( ('.', [os.path.join('.', 'demo_model_3.dat')]) )
+data_files.append( ('.', [os.path.join('.', 'direfl.ico')]) )
+data_files.append( ('.', [os.path.join('.', 'LICENSE-direfl.txt')]) )
+data_files.append( ('.', [os.path.join('.', 'qrd1.refl')]) )
+data_files.append( ('.', [os.path.join('.', 'qrd2.refl')]) )
+data_files.append( ('.', [os.path.join('.', 'surround_air_4.refl')]) )
+data_files.append( ('.', [os.path.join('.', 'surround_d2o_4.refl')]) )
+data_files.append( ('.', [os.path.join('.', 'README-direfl.txt')]) )
+data_files.append( ('.', [os.path.join('.', 'splash.png')]) )
 
-# Specify required packages.
-packages = ['numpy', 'scipy', 'matplotlib', 'pytz']
+# Specify required packages to bundle in the executable image.
+packages = ['matplotlib', 'numpy', 'scipy', 'pytz']
 
-# Specify files to include and exclude.
+# Specify files to include in the executable image.
 includes = []
 
+# Specify files to exclude from the executable image.
 excludes = ['Tkinter', 'PyQt4']
 
 dll_excludes = ['MSVCR71.dll',
@@ -154,27 +132,104 @@ dll_excludes = ['MSVCR71.dll',
                 'QtGui4.dll',
                 'QtCore4.dll']
 
+class Target():
+    """This class stores metadata about the distribution."""
+
+    def __init__(self, **kw):
+        self.__dict__.update(kw)
+        self.version = version
+        self.company_name = "University of Maryland"
+        self.copyright = "BSD style copyright"
+
 # This will create a console window when the DiRefl application is run.  The
 # application will then create a separate GUI application window.
-target_console_client = Target(
-    description = 'DiRefl: Direct Inversion and Phase Reconstruction',
-    script = './direfl.py',
+console_client = Target(
+    name = "DiRefl",
+    description = 'Direct Inversion and Phase Reconstruction',
+    script = os.path.join('.', 'direfl.py'),
     dest_base = "direfl",
     other_resources = [(24, 1, manifest)])
 
 # Now do the work to create a standalone distribution using py2exe.
 setup(windows=[],
-      console=[target_console_client],
+      console=[console_client],
       options={'py2exe': {
-                          'dll_excludes': dll_excludes,
                           'packages': packages,
                           'includes': includes,
                           'excludes': excludes,
+                          'dll_excludes': dll_excludes,
                           'compressed': 1,
                           'optimize': 0,
                           'bundle_files': 1 # bundle python25.dll in executable
                          }
               },
       zipfile=None,                         # bundle library.zip in executable
-      data_files=data_files
+      data_files=data_files                 # list of files to put in dist\
      )
+
+#==============================================================================
+# This section is for reference only when using older versions of matplotlib.
+
+# The location of mpl-data files has changed across releases of matplotlib.
+# Furthermore, matplotlib.get_py2exe_datafiles() had problems prior to version
+# 0.99 (see link below for details), so alternative ways had to be used.
+# The various techniques shown below for obtaining matplotlib auxiliary files
+# (and previously used by this project) was adapted from the examples and
+# discussion on http://www.py2exe.org/index.cgi/MatPlotLib.
+#
+# The following technique worked for matplotlib 0.91.2.
+# Note that glob '*.*' will not find files that have no file extension.
+'''
+import glob
+
+data_files = []
+matplotlibdatadir = matplotlib.get_data_path()
+mpl_lst = ('mpl-data', glob.glob(os.path.join(matplotlibdatadir, '*.*')))
+data_files.append(mpl_lst)
+mpl_lst = ('mpl-data', [os.path.join(matplotlibdatadir, 'matplotlibrc')])
+data_files.append(mpl_lst)  # pickup file missed by glob
+mpl_lst = (r'mpl-data\fonts',
+           glob.glob(os.path.join(matplotlibdatadir, r'fonts\*.*')))
+data_files.append(mpl_lst)
+mpl_lst = (r'mpl-data\images',
+           glob.glob(os.path.join(matplotlibdatadir, r'images\*.*')))
+data_files.append(mpl_lst)
+'''
+
+# The following technique worked for matplotlib 0.98.5.
+# Note that glob '*.*' will not find files that have no file extension.
+'''
+import glob
+
+data_files = []
+matplotlibdatadir = matplotlib.get_data_path()
+mpl_lst = ('mpl-data', glob.glob(os.path.join(matplotlibdatadir, '*.*')))
+data_files.append(mpl_lst)
+mpl_lst = ('mpl-data', [os.path.join(matplotlibdatadir, 'matplotlibrc')])
+data_files.append(mpl_lst)  # pickup file missed by glob
+mpl_lst = (r'mpl-data\fonts\afm',
+           glob.glob(os.path.join(matplotlibdatadir, r'fonts\afm\*.*')))
+data_files.append(mpl_lst)
+mpl_lst = (r'mpl-data\fonts\pdfcorefonts',
+           glob.glob(os.path.join(matplotlibdatadir, r'fonts\pdfcorefonts\*.*')))
+data_files.append(mpl_lst)
+mpl_lst = (r'mpl-data\fonts\ttf',
+           glob.glob(os.path.join(matplotlibdatadir, r'fonts\ttf\*.*')))
+data_files.append(mpl_lst)
+mpl_lst = (r'mpl-data\images',
+           glob.glob(os.path.join(matplotlibdatadir, r'images\*.*')))
+data_files.append(mpl_lst)
+'''
+
+# The following technique worked for matplotlib 0.98 and 0.99.
+'''
+from distutils.filelist import findall
+
+data_files = []
+matplotlibdatadir = matplotlib.get_data_path()
+matplotlibdata = findall(matplotlibdatadir)
+
+for f in matplotlibdata:
+    dirname = os.path.join('mpl-data', f[len(matplotlibdatadir)+1:])
+    data_files.append((os.path.split(dirname)[0], [f]))
+'''
